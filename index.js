3016f2655f80ba419572ac88ce3f76284788b826
lorenzo <lorenzo@frescofrigo.it>
2020-10-21 10:34:52 +0200

    fix overlay crash

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ import {
   requireNativeComponent,
   StyleSheet,
   TouchableWithoutFeedback,
-  Animated
-} from "react-native";
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-var emitter = require("tiny-emitter/instance");
+  Animated,
+} from 'react-native'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+var emitter = require('tiny-emitter/instance')
 
-const { SajjadBlurOverlay } = NativeModules;
+const { SajjadBlurOverlay } = NativeModules
 var iface = {
-  name: "BlurView",
+  name: 'BlurView',
   propTypes: {
     ...View.propTypes,
     brightness: PropTypes.any,
@@ -21,56 +21,56 @@ var iface = {
     downsampling: PropTypes.number,
     blurStyle: PropTypes.string,
     vibrant: PropTypes.bool,
-    idBlur: PropTypes.string
-  }
-};
+    idBlur: PropTypes.string,
+  },
+}
 var RCTSajjadBlurOverlay = Platform.select({
-  ios: () => requireNativeComponent("SajjadBlurOverlay", iface),
-  android: () => requireNativeComponent("RCTSajjadBlurOverlay", iface)
-})();
+  ios: () => requireNativeComponent('SajjadBlurOverlay', iface),
+  android: () => requireNativeComponent('RCTSajjadBlurOverlay', iface),
+})()
 export default class BlurOverlay extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       showBlurOverlay: false,
-      fadeIn: new Animated.Value(0)
-    };
-    this._openOverlay = this.openOverlay.bind(this);
-    this._closeOverlay = this.closeOverlay.bind(this);
+      fadeIn: new Animated.Value(0),
+    }
+    this._openOverlay = this.openOverlay.bind(this)
+    this._closeOverlay = this.closeOverlay.bind(this)
   }
   openOverlay() {
     this.setState(
       {
         showBlurOverlay: true,
-        fadeIn: new Animated.Value(0)
+        fadeIn: new Animated.Value(0),
       },
       () => {
         Animated.timing(this.state.fadeIn, {
           toValue: 1,
           duration: 500,
-          useNativeDriver: true
-        }).start();
+          useNativeDriver: true,
+        }).start()
       }
-    );
+    )
   }
   closeOverlay() {
     Animated.timing(this.state.fadeIn, {
       toValue: 0,
       duration: 500,
-      useNativeDriver: true
-    }).start(() => this.setState({ showBlurOverlay: false }));
+      useNativeDriver: true,
+    }).start(() => this.setState({ showBlurOverlay: false }))
   }
   componentDidMount() {
-    emitter.on("drawer-open" + this.props.idBlur, this._openOverlay);
-    emitter.on("drawer-close" + this.props.idBlur, this._closeOverlay);
+    emitter.on('drawer-open' + this.props.idBlur, this._openOverlay)
+    emitter.on('drawer-close' + this.props.idBlur, this._closeOverlay)
   }
   componentWillUnmount() {
-    emitter.off("drawer-open" + this.props.idBlur, this._openOverlay);
-    emitter.off("drawer-close" + this.props.idBlur, this._closeOverlay);
+    emitter.off('drawer-open' + this.props.idBlur, this._openOverlay)
+    emitter.off('drawer-close' + this.props.idBlur, this._closeOverlay)
   }
 
   render() {
-    const { children } = this.props;
+    const { children } = this.props
     return this.state.showBlurOverlay ? (
       <Animated.View style={[{ opacity: this.state.fadeIn }, styles.style]}>
         <TouchableWithoutFeedback
@@ -87,13 +87,13 @@ export default class BlurOverlay extends React.Component {
           </RCTSajjadBlurOverlay>
         </TouchableWithoutFeedback>
       </Animated.View>
-    ) : null;
+    ) : null
   }
 }
 
 const styles = StyleSheet.create({
   style: {
-    position: "absolute",
+    position: 'absolute',
     flex: 1,
     left: 0,
     top: 0,
@@ -102,12 +102,12 @@ const styles = StyleSheet.create({
     //  resizeMode: 'cover',
     width: null,
     height: null,
-    zIndex: 999
-  }
-});
+    zIndex: 999,
+  },
+})
 export function openOverlay(idBlur) {
-  emitter.emit("drawer-open" + idBlur);
+  emitter.emit('drawer-open' + idBlur)
 }
 export function closeOverlay(idBlur) {
-  emitter.emit("drawer-close" + idBlur);
+  emitter.emit('drawer-close' + idBlur)
 }
